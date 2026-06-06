372138e00ed64f934b48b13b8f05f8893497c743
schlami <75256911+schlami@users.noreply.github.com>
2026-06-06 16:15:20 +0200

    Update devices.py

--- custom_components/tuya_ble/devices.py
+++ custom_components/tuya_ble/devices.py
@@ -352,7 +352,6 @@ devices_database: dict[str, TuyaBLECategoryInfo] = {
             **dict.fromkeys(
                 [
                     "ludzroix",
-                    "wg84u6ez",
                     "isk2p555",
                     "gumrixyt",
                     "uamrw6h3",
@@ -393,6 +392,7 @@ devices_database: dict[str, TuyaBLECategoryInfo] = {
     ),
     "jtmspro": TuyaBLECategoryInfo(
         products={
+            "wg84u6ez", TuyaBLEProductInfo(name="Martins SmartLock", lock=1),
             "xicdxood": TuyaBLEProductInfo(name="Raycube K7 Pro+", lock=1),
             "oyqux5vv": TuyaBLEProductInfo(name="LA-01 Smart lock", lock=1),
             "rlyxv7pe": TuyaBLEProductInfo(name="A1 PRO MAX", lock=1),
